5b49b711a676a4f93f9d3771cad1a3017de1b356
Shaquille Galimba <shaqrivergalimba24@gmail.com>
2019-11-21 05:32:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- cart.push({itemName:item, itemPrice:Math.floor(Math.random() * 10) + 1});
+ cart.push({itemName:item, itemPrice: (Math.floor(Math.random() * 10) + 1}));
  return `${itemName} has been added to your cart.`
 }
 
