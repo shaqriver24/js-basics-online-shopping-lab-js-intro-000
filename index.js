0742336c2f633051d96c45e787729a468711e281
Shaquille Galimba <shaqrivergalimba24@gmail.com>
2019-11-21 06:40:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -66,10 +66,7 @@ function placeOrder(cardNumber) {
   if(cardNumber === undefined) {
     return "Sorry, we don't have a credit card on file for you."
   }
-  else {
-    let total = total();
-    cart = [];
+  cart.length = 0;
 
-    return `Your total cost is $${total}, which will be charged to card ${cardNumber}}
-  }
+  return `Your total cost is $${total()}, which will be charged to card ${cardNumber}}  
 }
