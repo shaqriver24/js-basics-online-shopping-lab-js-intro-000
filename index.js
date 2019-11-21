302f171e6d2ea124cc8342d878d3d05b19b78463
Shaquille Galimba <shaqrivergalimba24@gmail.com>
2019-11-21 06:32:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -63,5 +63,7 @@ function removeFromCart(item) {
 }
 
 function placeOrder(cardNumber) {
-  // write your code here
+  if(cardNumber === null) {
+    return "Sorry, we don't have a credit card on file for you."
+  }
 }
